e29fe59cb7193899cfbf6f018405045a48849725
ivi.berberi <23117123>
2025-01-16 14:23:14 +0100

    Commit section 12 - Reactive forms

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,11 +1,11 @@
-import {Component, inject, input, OnInit, output} from '@angular/core';
-import {Member} from '../../_models/member';
-import {FileUploader, FileUploadModule} from 'ng2-file-upload';
-import {DecimalPipe, NgClass, NgFor, NgIf, NgStyle} from '@angular/common';
-import {AccountService} from '../../_services/account.service';
-import {environment} from '../../../environments/environment';
-import {MembersService} from '../../_services/members.service';
-import {Photo} from '../../_models/photo';
+import { Component, inject, input, OnInit, output } from '@angular/core';
+import { Member } from '../../_models/member';
+import { FileUploader, FileUploadModule } from 'ng2-file-upload';
+import { DecimalPipe, NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
+import { AccountService } from '../../_services/account.service';
+import { environment } from '../../../environments/environment';
+import { MembersService } from '../../_services/members.service';
+import { Photo } from '../../_models/photo';
 
 @Component({
   selector: 'app-photo-editor',
@@ -36,7 +36,7 @@ export class PhotoEditorComponent implements OnInit {
   deletePhoto(photo: Photo) {
     this.memberService.deletePhoto(photo).subscribe({
       next: _ => {
-        const updatedMember = {...this.member()};
+        const updatedMember = { ...this.member() };
         updatedMember.photos = updatedMember.photos.filter(x => x.id !== photo.id);
         this.memberChange.emit(updatedMember);
       }
@@ -51,7 +51,7 @@ export class PhotoEditorComponent implements OnInit {
           user.photoUrl = photo.url;
           this.accountService.setCurrentUser(user)
         }
-        const updatedMember = {...this.member()}
+        const updatedMember = { ...this.member() }
         updatedMember.photoUrl = photo.url;
         updatedMember.photos.forEach(p => {
           if (p.isMain) p.isMain = false;
@@ -77,9 +77,22 @@ export class PhotoEditorComponent implements OnInit {
     }
     this.uploader.onSuccessItem = (item, response, status, headers) => {
       const photo = JSON.parse(response);
-      const updatedMember = {...this.member()}
+      const updatedMember = { ...this.member() }
       updatedMember.photos.push(photo);
       this.memberChange.emit(updatedMember);
+      if (photo.isMain) {
+        const user = this.accountService.currentUser();
+        if (user) {
+          user.photoUrl = photo.url;
+          this.accountService.setCurrentUser(user)
+        }
+        updatedMember.photoUrl = photo.url;
+        updatedMember.photos.forEach(p => {
+          if (p.isMain) p.isMain = false;
+          if (p.id === photo.id) p.isMain = true;
+        });
+        this.memberChange.emit(updatedMember)
+      }
     }
   }
 }
