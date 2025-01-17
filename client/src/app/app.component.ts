9a32d324f2b3241c688cccc9f4afcb8836d2f7be
ivi.berberi <23117123>
2025-01-17 21:45:09 +0100

    Commit section 14 - Likes features

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,8 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import {NavComponent} from './nav/nav.component';
-import {AccountService} from './_services/account.service';
-import {NgxSpinnerComponent} from 'ngx-spinner';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { AccountService } from './_services/account.service';
+import { NgxSpinnerComponent } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
@@ -22,6 +22,6 @@ export class AppComponent implements OnInit {
     const userString = localStorage.getItem('user');
     if (!userString) return;
     const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
+    this.accountService.setCurrentUser(user);
   }
 }
